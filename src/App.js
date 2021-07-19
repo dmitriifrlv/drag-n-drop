a676ea495efc2b0784e10070ab100fe4d406b2a4
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-07-19 20:25:25 +0500

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,139 +1,27 @@
-import { useState } from 'react';
 import './App.css';
-import { DragDropContext,  Droppable, Draggable } from 'react-beautiful-dnd'
-import uuid from 'uuid/v4'
-
-const items = [
-  {id: uuid(), content: 'First Task'},
-  {id: uuid(), content: 'Second Task'}
-]
-
-const columnsData = 
-  {
-    [uuid()]: {
-      name: 'Bucket List',
-      items: items
-    },
-    [uuid()]: {
-      name: 'In Progress',
-      items: []
-    },
-    [uuid()]: {
-      name: 'Done',
-      items: []
-    }
-  }
-
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) {
-    return
-  }
-  const {source, destination} = result;
-  console.log(result)
-  console.log(source)
-  console.log(destination)
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId]
-    const destinationColumn = columns[destination.droppableId]
-    const sourceItems = [...sourceColumn.items]
-    const destItems = [...destinationColumn.items]
-    const [removed] = sourceItems.splice(source.index, 1)
-    console.log(removed)
-    destItems.splice(destination.index, 0, removed)
-    setColumns({...columns,[source.droppableId]:{
-      ...sourceColumn,
-      items:sourceItems
-    },
-    [destination.droppableId] : {
-      ...destinationColumn,
-      items:destItems
-    }
-  })
-  } else {
-    const column = columns[source.droppableId]
-    const copiedItems = [...column.items]
-    const [removed] = copiedItems.splice(source.index, 1)
-    copiedItems.splice(destination.index, 0, removed)
-    setColumns({
-      ...columns, 
-      [source.droppableId]:{
-        ...column,
-        items: copiedItems
-      }
-    })
-  }
-}
-
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Kanban from './Components/Kanban.jsx';
+import Mix from './Components/Mix.jsx'
 function App() {
-  const [columns, setColumns] = useState(columnsData)
+
   return (
     <div className="App">
-      <DragDropContext 
-      // onDragEnd={result=>console.log(result)}
-      onDragEnd={result => onDragEnd(result, columns, setColumns)}
-      >
-        {Object.entries(columns).map(([id, column]) => {
-          return (
-            <div style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center"
-            }}
-            key={id}
-            >
-              <h2>{column.name}</h2>
-              <div style={{ margin: 8 }}>
-            <Droppable droppableId={id} key={id}>
-              {
-                (provided, snapshot) => {
-                  return (
-                    <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{
-                      background: snapshot.isDraggingOver
-                        ? "lightblue"
-                        : "lightgrey",
-                      padding: 4,
-                      width: 250,
-                      minHeight: 500
-                    }}
-                    >
-                      {column.items.map((item, index)=> {
-                        return (
-                          <Draggable key={item.id} draggableId={item.id} index={index}>
-                            {(provided, snapshot) => {
-                              return (
-                                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
-                                style={{
-                                  userSelect:'none',
-                                  padding:16,
-                                  margin: '0 0 8px 0',
-                                  minHeight: '50px',
-                                  backgroundColor: snapshot.isDragging ? '#263b4a' : '#456C86',
-                                  color: 'white',
-                                  ...provided.draggableProps.style
-                                }}
-                                >
-                                  {item.content}
-                                </div>
-                              )
-                            }}
-                          </Draggable>
-                        )
-                      })}
-                      {provided.placeholder}
-                    </div>
-                  )
-                }
-              }
-            </Droppable>
-            </div>
-            </div>
-          )
-        })}
-      </DragDropContext>
-    </div>
+      <BrowserRouter>
+      {/* <ul className="nav">
+      <Link to="/kanban">Kanban</Link>
+      <Link to="/mix">Mix</Link>
+      </ul> */}
+      <Switch>
+        <Route exact path='/kanban' component={Kanban}/>
+        <Route exact path='/mix' component={Mix}/>
+      </Switch>
+      </BrowserRouter>
+          </div>
   );
 }
 
---
+++ src/Components/Mix.jsx
@@ -0,0 +1,5 @@
+export default function Mix() {
+  return(
+    <div>hi</div>
+  )
+}
