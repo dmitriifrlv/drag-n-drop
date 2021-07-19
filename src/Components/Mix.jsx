f39966852ccea76ca147b2ac6aea317d067e01e1
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-07-19 21:14:03 +0500

    SAVED PROGRESS

--- src/Components/Mix.jsx
+++ src/Components/Mix.jsx
@@ -1,5 +1,151 @@
+import './Mix.css'
+import { useState } from 'react';
+import { DragDropContext,  Droppable, Draggable } from 'react-beautiful-dnd'
+import uuid from 'uuid/v4'
+const operators = [
+  {id: uuid(), content: 'Addition'},
+  {id: uuid(), content: 'Subtraction'},
+  {id: uuid(), content: 'Multiplication'},
+  {id: uuid(), content: 'Division'}
+]
+const operands = [
+  {id: uuid(), content: 'Operand A'},
+  {id: uuid(), content: 'Operand B'},
+  {id: uuid(), content: 'Operand C'},
+]
+const columnsData = 
+  {
+    'operators': {
+      name: 'Operators',
+      items: operators
+    },
+    'operands': {
+      name: 'Operands',
+      items: operands
+    },
+    'constructor': {
+      name: 'Constructor',
+      items: []
+    }
+  }
 export default function Mix() {
+  const [columns, setColumns] = useState(columnsData)
+  const onDragEnd = (result, columns, setColumns) => {
+    console.log(result)
+    // if (!result.destination) {
+    //   return
+    // }
+    // const {source, destination} = result;
+    // console.log(result)
+    // console.log(source)
+    // console.log(destination)
+    // if (source.droppableId !== destination.droppableId) {
+    //   const sourceColumn = columns[source.droppableId]
+    //   const destinationColumn = columns[destination.droppableId]
+    //   const sourceItems = [...sourceColumn.items]
+    //   const destItems = [...destinationColumn.items]
+    //   const [removed] = sourceItems.splice(source.index, 1)
+    //   console.log(removed)
+    //   destItems.splice(destination.index, 0, removed)
+    //   setColumns({...columns,[source.droppableId]:{
+    //     ...sourceColumn,
+    //     items:sourceItems
+    //   },
+    //   [destination.droppableId] : {
+    //     ...destinationColumn,
+    //     items:destItems
+    //   }
+    // })
+    // } else {
+    //   const column = columns[source.droppableId]
+    //   const copiedItems = [...column.items]
+    //   const [removed] = copiedItems.splice(source.index, 1)
+    //   copiedItems.splice(destination.index, 0, removed)
+    //   setColumns({
+    //     ...columns, 
+    //     [source.droppableId]:{
+    //       ...column,
+    //       items: copiedItems
+    //     }
+    //   })
+    // }
+  }
   return(
-    <div>hi</div>
+    <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
+  
+    <div className="container">
+      <div className="functionContainer">
+       
+        <Droppable droppableId='operators' key='operators'>
+              {
+                (provided, snapshot) => {
+                  return (
+                    <div
+                    className="operatorsContainer"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "transparent",
+                    }}
+                    ></div>)}}
+                    </Droppable> 
+
+        <Droppable droppableId='constructor' key='constructor'>
+              {
+                (provided, snapshot) => {
+                  return (
+                    <div
+                    className="functionConstructorContainer"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "transparent",
+                    }}
+                    ></div>)}}
+                    </Droppable> 
+      </div>
+      <Droppable droppableId='operands' key='operands'>
+              {
+                (provided, snapshot) => {
+                  return (
+                    <div
+                    className="operandContainer"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "transparent",
+                    }}
+                    >
+                      {operands.map((item, index)=>{
+                        return(
+                          <Draggable key={item.id} draggableId={item.id} index={index}>
+                            {(provided, snapshot) => {
+                              return (
+                                <div
+                                className='operand'
+                                 ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                                style={{
+                                  backgroundColor: snapshot.isDragging ? '#263b4a' : '#456C86',
+                     
+                                  ...provided.draggableProps.style
+                                }}
+                                >
+                                  {item.content}
+                                </div>
+                              )
+                            }}
+                          </Draggable>
+                        )
+                      })}
+                    </div>)}}
+                    </Droppable> 
+    </div>
+    </DragDropContext>
   )
 }
